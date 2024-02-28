8562542f981f8cf0ce4928f3c26e7b6de9636ea7
Aishah <aishah122005@gmail.com>
2024-02-28 09:40:02 -0600

    Initial Commit

---
+++ Za Hunter Hackwich/ContentView.swift
@@ -0,0 +1,7 @@
+//
+//  ContentView.swift
+//  Za Hunter Hackwich
+//
+//  Created by Aishah Siraj on 2/21/24.
+//
+
---
+++ Za Hunter Hackwich/Za_Hunter_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_Hunter_HackwichApp.swift
+//  Za Hunter Hackwich
+//
+//  Created by Aishah Siraj on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Za_Hunter_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

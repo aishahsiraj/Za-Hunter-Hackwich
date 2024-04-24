54c776fcb2767b721d2f7627cd7ca91642fbbc05
Aishah <aishah122005@gmail.com>
2024-04-24 10:00:46 -0500

    finished

--- Za Hunter Hackwich/LocationManager.swift
+++ Za Hunter Hackwich/LocationManager.swift
@@ -2,65 +2,7 @@
 //  LocationManager.swift
 //  Za Hunter Hackwich
 //
-//  Created by Aishah Siraj on 2/28/24.
+//  Created by Aishah Siraj on 4/11/24.
 //
 
 import Foundation
-
-import SwiftUI
-import MapKit
-
-struct ContentView: View {
-    @State private var places = [Place]()
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(
-            latitude: 42.15559,
-            longitude: -88.14692),
-        span: MKCoordinateSpan(
-            latitudeDelta: 0.05,
-            longitudeDelta: 0.05)
-    )
-    @StateObject var locationManager = LocationManager()
-    @State private var userTrackingMode: MapUserTrackingMode = .follow
-    var body: some View {
-        Map(
-            coordinateRegion: $region,
-            interactionModes: .all,
-            showsUserLocation: true,
-            userTrackingMode: $userTrackingMode,
-            annotationItems: places) {place in
-                MapPin(coordinate: place.annotation.coordinate)
-            }
-            .onAppear(perform: {
-                performSearch(item: "Pizza")
-            })
-        
-    }
-    func performSearch(item: String) {
-        let searchRequest = MKLocalSearch.Request()
-        searchRequest.naturalLanguageQuery = item
-        searchRequest.region = region
-        let search = MKLocalSearch(request: searchRequest)
-        search.start{ (response, error) in
-            if let response = response {
-                for mapItem in response.mapItems {
-                    let annotation = MKPointAnnotation()
-                    annotation.coordinate = mapItem.placemark.coordinate
-                    annotation.title = mapItem.name
-                    places.append(Place(annotation: annotation, mapItem: mapItem))
-                }
-            }
-        }
-    }
-}
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-struct Place: Identifiable {
-    let id = UUID()
-    let annotation: MKPointAnnotation
-    let mapItem: MKMapItem
-}
---
+++ Za Hunter Hackwich/Pizza Hunter/Pizza Hunter/Pizza_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_HunterApp.swift
+//  Pizza Hunter
+//
+//  Created by Aishah Siraj on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
